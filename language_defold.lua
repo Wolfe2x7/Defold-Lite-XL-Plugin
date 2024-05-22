9d8c37df0acfe1306a36a41387ce4ba38d8b4e98
Wolfe2x7 <72348938+Wolfe2x7@users.noreply.github.com>
2024-05-22 12:05:16 -0500

    Add .gui_script to language_defold.lua

--- language_defold.lua
+++ language_defold.lua
@@ -3,7 +3,7 @@ local syntax = require "core.syntax"
 
 syntax.add {
   name = "Defold Script",
-  files = { "%.script$", "%.render_script$", "%.buffer$", },
+  files = { "%.script$", "%.gui_script$", "%.render_script$", "%.buffer$", },
   comment = "--",
   block_comment = { "--[[", "]]" },
   patterns = {
